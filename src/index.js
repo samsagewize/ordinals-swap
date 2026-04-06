c33fa17af39c6e418c298b1bdbe5a8017f8eaaeb
samsagewize <114315604+samsagewize@users.noreply.github.com>
2026-04-06 17:27:31 -0500

    fix: move ethereum conflict fix to index.html

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-// Suppress EVM wallet extension conflicts (MetaMask etc)
-if (typeof window !== 'undefined') {
-  try {
-    Object.defineProperty(window, 'ethereum', { writable: true, configurable: true });
-  } catch (_) {}
-}
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
